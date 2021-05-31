f384547df3bd30a9c7262639eee4ff592ab0f71d
Guillermo Acosta <guillermo222@att.net>
2021-05-31 12:08:45 -0700

    Managing Recipes in a Recipe Service

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,3 +1,15 @@
+import { Recipe } from './recipe.model';
+
 export class RecipeService {
-    
+    // private so recipes can't be accessed from outside
+    private recipes: Recipe[] = [
+        new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
+        new Recipe('Another Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+    ];
+
+    // So recipes can be accessed from outside
+    getRecipes() {
+        // slice is used to get a copy, and really make sure we don't access the array stored from outside
+        return this.recipes.slice();
+    }
 }
